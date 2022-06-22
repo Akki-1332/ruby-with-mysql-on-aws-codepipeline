b3e1b95723078e2a2b4c6bdbfc32b174f820fa7f
akki <aakash.choudhary.ongraph@gmail.com>
2022-06-22 17:01:24 +0530

    new

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,15 +1,8 @@
 #!/usr/bin/bash
 
-#sudo yum update -y
-#sudo yum install gcc git -y 
-#sudo amazon-linux-extras install ruby3.0 -y
-#sudo yum install ruby-devel -y
-#sudo gem install rails
-
-rbenv install 3.1.0
-rbenv global 3.1.0
-echo "gem: --no-document" > ~/.gemrc
-gem install rails
-rbenv rehash
-
+sudo yum update -y
+sudo yum install gcc git -y 
+sudo amazon-linux-extras install ruby3.0 -y
+sudo yum install ruby-devel -y
+sudo gem install rails
 sudo yum install mysql-devel -y
