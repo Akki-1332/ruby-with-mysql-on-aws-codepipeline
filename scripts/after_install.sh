1f748088d708e02be5218fed12023e08c969af5c
akki <aakash.choudhary.ongraph@gmail.com>
2022-06-22 14:12:58 +0530

    changes:

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-sudo chown -R ec2-user:ec2-user ruby_app/
-#sudo chmod 666 /home/ec2-user/Gemfile.lock
+sudo chown -R ec2-user:ec2-user /home/ec2-user/ruby_app/
+#sudo chmod 666 /home/ec2-user/ruby_app/Gemfile.lock
 gem install puma -v 5.6.4
 cd /home/ec2-user/ruby_app/ && bundle install
