733b4cd1db46753effce0d00119cab6ccb7ad0a3
akki <aakash.choudhary.ongraph@gmail.com>
2022-06-23 14:39:54 +0530

    script modified start server

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
 export RAILS_DB_PASS=Akki@1332
-cd /home/ec2-user/ruby_app/  && rails s -b 0.0.0.0 &
-sleep 5
-date
-bash
+cmd="cd /home/ec2-user/ruby_app/  && rails s -b 0.0.0.0"
+"${cmd}" &>/dev/null & disown;
